0fbb2c13ac152cc5079dc3d5c82958c37b5bc4d8
Damian Gunadasa <damian718@gmail.com>
2026-02-12 22:51:49 -0500

    Runs locally, no data yet

--- src/main/java/com/damian/animestream/AnimestreamApplication.java
+++ src/main/java/com/damian/animestream/AnimestreamApplication.java
@@ -1,5 +1,12 @@
 package com.damian.animestream;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AnimestreamApplication {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(AnimestreamApplication.class, args);
+    }
 }
+
